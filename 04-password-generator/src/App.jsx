785115451b8544945423c3bc7dbfacfce7df591f
AanandShubham <www.subham.anand@gmail.com>
2025-02-20 23:27:44 +0530

    creating a password generator with to learn about useRef,useEffect and useCallback hooks

---
+++ 04-password-generator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='bg-amber-600'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
