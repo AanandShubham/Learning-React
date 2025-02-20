ff9eedab7729e342db171c035c9ba3d3d9733a82
AanandShubham <www.subham.anand@gmail.com>
2025-02-20 23:29:42 +0530

    creating passwordGenerator app to learn the concept of hooks like useRef  ,  useEffect  ,  useCallback

--- 04-password-generator/src/App.jsx
+++ 04-password-generator/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1 className='bg-amber-600'>Password Generator</h1>
+      <div className="w-screen h-screen flex justify-center p-4 items-center bg-neutral-700">
+        <div className="w-fit h-fit p-5 bg-neutral-400">
+          <h2 className="">Password Generator</h2>
+
+          <div className="w-full h-full">
+            <input className="bg-white min-w-[400px]" type="text" />
+            <button className="bg-blue-300">copy</button>
+          </div>
+
+
+          <div className="flex gap-2">
+            <input
+              className="w-30"
+              type="range" />
+            <p>length :{} </p>
+            <input type="checkbox" name="" id="" />
+            <p>Numbers</p>
+            <input type="checkbox" name="" id="" />
+            <p>SpecialCharacker</p>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
