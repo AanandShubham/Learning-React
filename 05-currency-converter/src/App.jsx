3b9b1272248ae655f9daf082be52e24e423c7b28
AanandShubham <www.subham.anand@gmail.com>
2025-02-24 00:12:39 +0530

    currency converter ui created

--- 05-currency-converter/src/App.jsx
+++ 05-currency-converter/src/App.jsx
@@ -6,9 +6,15 @@ function App() {
 
   return (
     <>
-      <div className='w-screen h-screen bg-neutral-700'>
-        <h2 className='bg-black text-center text-amber-200 text-4xl'>Currency Converter</h2>
-        <CurrencyCard/>
+      <div className='w-screen h-screen bg-neutral-400 p-4 flex items-center justify-center'>
+
+        <div className='w-fit h-fit bg-neutral-300 rounded-md p-4 '>
+          <h2 className='bg-neutral-700 text-center text-neutral-200 text-4xl rounded-xl'>Currency Converter</h2>
+          <CurrencyCard />
+          <CurrencyCard />
+          <button className='w-fit p-2 rounded-sm absolute top-1/2 left-1/2 bg-amber-200'>swap</button>
+          <button className='w-full mt-3 p-1 rounded-xl bg-neutral-200'>Convert</button>
+        </div>
       </div>
     </>
   )
--- 05-currency-converter/src/components/CurrencyCard.jsx
+++ 05-currency-converter/src/components/CurrencyCard.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 
 function CurrencyCard() {
-  return (
-    <div>
-        <h2 className='border-4 border-pink-400'>card</h2>
-        
-    </div>
-  )
+
+    const [value,setvalue] = useState()
+  
+    return (
+        <div className='flex gap-5 m-3 bg-neutral-100 w-fit p-4 rounded-md h-fit'>
+           <div className='flex flex-col'> 
+                <label htmlFor="">From</label>
+                <input type="number" name="" id="" />
+           </div>
+           <div className='flex flex-col'>
+                <label htmlFor="">Currency Type</label>
+                <select name="" id="" value={value}  onChange={(e)=>setvalue(e.target.value)}>
+                    <option value="usd">usd</option>
+                    <option value="inr">inr</option>
+                </select>
+           </div>
+        </div>
+    )
 }
 
 export default CurrencyCard
